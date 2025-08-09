7dc2200f309afd7d31cd80747b45501f3a81c4b1
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 01:22:03 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/oauth2server/main.go
@@ -0,0 +1,23 @@
+// cmd/oauth2server/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "oauth2server/internal/oauth2server"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := oauth2server.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
